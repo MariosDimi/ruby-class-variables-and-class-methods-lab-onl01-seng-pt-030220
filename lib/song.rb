28aa9ca48667739df5695d32c272aaed3c3680cd
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2020-08-01 16:56:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,60 +1,55 @@
+  
+require 'pry'
+
 class Song
-  
   attr_accessor :name, :artist, :genre
-  
-  @@song_count = 0
+
+  @@count = 0
+  @@genres = []
   @@artists = []
-  @@genre = []
-  
-  def initialize(name, artist, genre)
-    @name = name
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
     @artist = artist
     @genre = genre
+    @@count += 1
     @@genres << genre
     @@artists << artist
-    @@songs_count += 1
   end
-  
+
   def self.count
-    @@songs_count
+    @@count
   end
-  
+
   def self.genres
-    @@genres.uniq
+    @@genres.uniq!
   end
-  
-  def self.artist
-    @@artists.uniq
+
+  def self.artists
+    @@artists.uniq!
   end
-  
+
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre]
-      genre_count[genre] += 1
-    else
-      genre_count[genre] = 1
+      if genre_count[genre] 
+        genre_count[genre] += 1
+      else 
+        genre_count[genre] = 1
+      end
     end
+    genre_count
   end
-  
+
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]
-      artist_count[artist] +=1
+      if artist_count[artist] 
+        artist_count[artist] += 1
       else
-      artist_count[artist] = 1
+        artist_count[artist] = 1
       end
     end
     artist_count
   end
-
 end
-
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
-mask_off = Song.new("Mask Off", "Future", "Rap")
-dance_macabre = Song.new("Dance Macabre", "Ghost", "Metal")
-if_you_have_ghosts = Song.new("If You Have Ghosts", "Ghost", "Metal")
-Song.genres
-p Song.artist_count
-p Song.genre_count
